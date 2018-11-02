0f4ecde78af6b929eda96fe318327fe05ce8400c
xray <creator@darkmattercreations.com>
2018-11-02 14:20:48 -0500

    Added functionality to get the link for the previous page of search results

--- lib/search_link_generator.rb
+++ lib/search_link_generator.rb
@@ -1,6 +1,7 @@
 require 'uri'
 require 'link'
 
+# Generates DuckDuckGo search links based on a serach query
 class LinkGenerator
   def initialize(link = Link)
     @link = link
@@ -14,7 +15,8 @@ class LinkGenerator
   def next_page(given_link)
     next_page_number = given_link.page + 1
     next_page_value = determine_page_number_value(next_page_number)
-    next_page_link = given_link.original_link + '&s=' + next_page_value + '&v=l&o=json&dc=' + next_page_value + '&api=%2Fd.js&kl=us-en'
+    next_page_link = "#{given_link.original_link}&s=#{next_page_value}"\
+                     "&v=l&o=json&dc=#{next_page_value}&api=%2Fd.js&kl=us-en"
     @link.new(
       link: next_page_link,
       original_link: given_link.original_link,
@@ -23,11 +25,18 @@ class LinkGenerator
     )
   end
 
+  def previous_page(given_link)
+    @link.new(
+      link: given_link.history.last.link,
+      original_link: given_link.original_link,
+      page: given_link.page - 1,
+      history: given_link.history.push(given_link)
+    )
+  end
+
   def determine_page_number_value(page_number)
     if page_number == 1
       false
-    elsif page_number == 2
-      30.to_s
     else
       (30 + ((page_number - 2) * 50)).to_s
     end
